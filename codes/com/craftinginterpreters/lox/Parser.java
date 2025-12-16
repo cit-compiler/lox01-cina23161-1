11a601564c3f777c1b0e4e74897ab3668b2a01a2
cina23161 <cina23161@g.nihon-u.ac.jp>
2025-12-16 11:55:17 +0900

    update lox01-cina23161-1

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -5,11 +5,21 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+private static class ParseError extends RuntimeException {}
+
 private final List<Token> tokens;
 private int current = 0;
 
 Parser(List<Token> tokens) {
     this.tokens = tokens;
+}
+
+Expr parse() {
+    try {
+    return expression();
+    } catch (ParseError error) {
+    return null;
+    }
 }
 
 private Expr expression() {
@@ -96,7 +106,6 @@ private Expr factor() {
 
     return expr;
 }
-
 private Expr unary() {
     if (match(BANG, MINUS)) {
     Token operator = previous();
@@ -106,5 +115,57 @@ private Expr unary() {
 
     return primary();
 }
+
+private Expr primary() {
+    if (match(FALSE)) return new Expr.Literal(false);
+    if (match(TRUE)) return new Expr.Literal(true);
+    if (match(NIL)) return new Expr.Literal(null);
+
+    if (match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    if (match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
+
+    throw error(peek(), "Expect expression.");
+  }
+
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+  private void synchronize() {
+    advance();
+
+    while (!isAtEnd()) {
+      if (previous().type == SEMICOLON) return;
+
+      switch (peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
+  }
+
 
 }
